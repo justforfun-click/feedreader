57687b7385408b43501499b68a15fce61cf7bd5a
xyb <xyb@xyb.name>
2020-07-26 17:05:07 -0700

    Support media rss. Can parse media:content now.

--- WebApi/Processors/FeedProcessor.cs
+++ WebApi/Processors/FeedProcessor.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
@@ -24,6 +25,9 @@ namespace FeedReader.WebApi.Processors
                 var xml = new XmlDocument();
                 xml.LoadXml(await new HttpClient().GetStringAsync(feed.Uri));
 
+                var xmlns = new XmlNamespaceManager(xml.NameTable);
+                xmlns.AddNamespace("media", "http://search.yahoo.com/mrss/");
+
                 // Parse channel. As spec, every feed has only one channel.
                 var channelNode = xml.SelectSingleNode("/rss/channel");
                 feed.Name = channelNode["title"].InnerText;
@@ -34,7 +38,7 @@ namespace FeedReader.WebApi.Processors
                 {
                     foreach (XmlNode itemNode in channelNode.SelectNodes("/rss/channel/item"))
                     {
-                        var item = ParseItem(itemNode);
+                        var item = ParseItem(itemNode, xmlns);
                         feed.Items.Add(item);
                     }
                 }
@@ -50,7 +54,7 @@ namespace FeedReader.WebApi.Processors
             return feed;
         }
 
-        private FeedItem ParseItem(XmlNode itemNode)
+        private FeedItem ParseItem(XmlNode itemNode, XmlNamespaceManager xmlns)
         {
             FeedItem item = new FeedItem();
             item.Title = itemNode["title"].InnerText;
@@ -58,15 +62,47 @@ namespace FeedReader.WebApi.Processors
             item.PubDate = DateTime.Parse(itemNode["pubDate"].InnerText);
             item.Content = itemNode["description"].InnerText;
 
-            // Can we find the picture in the content?
-            var match = ImgRegex.Match(item.Content);
-            if (match.Success)
+            // Find media content, standard: media rss, https://www.rssboard.org/media-rss
+            string imgUrl = null;
+            var mediaContents = itemNode.SelectNodes("media:group/media:content", xmlns);
+            if (mediaContents != null)
             {
-                var uri = match.Groups[1].Value;
-                if (Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                foreach (XmlNode mediaContent in mediaContents)
                 {
-                    item.TopicPictureUri = uri;
+                    var attributes = mediaContent.Attributes;
+                    var medium = attributes["medium"]?.InnerText;
+                    if (medium == "image")
+                    {
+                        // is default?
+                        if (attributes["isDefault"]?.InnerText.Trim().ToLower() == "true")
+                        {
+                            imgUrl = attributes["url"].InnerText;
+                            break;
+                        }
+                        else if (string.IsNullOrWhiteSpace(imgUrl))
+                        {
+                            imgUrl = attributes["url"].InnerText;
+                        }
+                    }
                 }
+            }
+
+            // Can we find the picture in the content?
+            if (string.IsNullOrWhiteSpace(imgUrl))
+            {
+                var match = ImgRegex.Match(item.Content);
+                if (match.Success)
+                {
+                    var uri = match.Groups[1].Value;
+                    if (Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                    {
+                        item.TopicPictureUri = uri;
+                    }
+                }
+            }
+            else
+            {
+                item.TopicPictureUri = imgUrl;
             }
 
             // Get summary.
