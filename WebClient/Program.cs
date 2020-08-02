669f81d8de8c3b156c7dbb58af4937bbe6c2d565
xyb <xyb@xyb.name>
2020-08-02 05:03:06 -0700

    Show pubDate in client timezone

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -2,6 +2,9 @@ using FeedReader.WebClient.Models;
 using FeedReader.WebClient.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System;
 using System.Threading.Tasks;
 
 namespace FeedReader.WebClient
@@ -20,9 +23,20 @@ namespace FeedReader.WebClient
             builder.Services.AddAuthorizationCore();
 
             var host = builder.Build();
+            DateTimeExtension.sTimezoneOffset = await host.Services.GetRequiredService<IJSRuntime>().InvokeAsync<int>("eval", "-new Date().getTimezoneOffset()");
             var localUser = host.Services.GetRequiredService<LocalUser>();
             await localUser.InitializeAsync();
             await host.RunAsync();
+        }
+    }
+
+    static class DateTimeExtension
+    {
+        public static int sTimezoneOffset;
+
+        public static DateTime ToClientLocalTime(this DateTime datetime)
+        {
+            return datetime.AddMinutes(sTimezoneOffset);
         }
     }
 }
