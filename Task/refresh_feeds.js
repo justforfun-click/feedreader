2bcb5ed63a0fbaa8721ef3d4056b0957d7d50b9d
xyb <xyb@xyb.name>
2020-11-24 11:21:55 -0800

    Update refresh_feeds.js, make it be run in a docker

--- Task/refresh_feeds.js
+++ Task/refresh_feeds.js
@@ -1,60 +1,95 @@
+const lockfile = require('proper-lockfile');
 const request = require("request-promise");
+const log4js = require("log4js");
 
-var args = process.argv.slice(2);
-if (!args) {
-    console.log("Usage node refresh_feeds.js --admin-key <xx> --api-base <xxxx>");
-    return;
-}
-
-var options = {
-    adminKey: null,
-    apiBase: null
+const options = {
+    adminKey: "local.test.admin.key",
+    apiBase: "http://localhost:7071/api",
+    logLevel: "info",
 };
 
-for (var i = 0; i < args.length; ++i) {
-    if (args[i] == "--admin-key") {
-        options.adminKey = args[++i];
-    } else if (args[i] == "--api-base") {
-        options.apiBase = args[++i];
-    }
-}
-if (!options.adminKey || !options.apiBase) {
-    console.log("Usage node refresh_feeds.js --admin-key <xx> --api-base <xxxx>");
-    return;
-}
+// Run as single instance.
+var log = null;
+lockfile
+.lock(`${__dirname}/refresh_feeds.lock`, {realpath: false})
+.then(() => main())
+.catch(e => { (log || console).log(e); process.exitCode = 1; });
 
-for (var i = options.apiBase.length - 1; i >= 0; --i) {
-    if (options.apiBase[i] != '/') {
-        options.apiBase = options.apiBase.substr(0, i + 1);
-        break;
-    }
-}
-
-(async ()=> {
-var data = await request(`${options.apiBase}/@admin/get-feed-uri-list`, {
-    headers: {
-        "AdminKey": options.adminKey
-    }
-});
-
-var feedURIs = JSON.parse(data);
-var failedCount = 0;
-console.log(`Refresh start, ${feedURIs.length} feeds need be refreshed.`);
-for (var i in feedURIs) {
-    var feedUri = feedURIs[i];
-    try {
-        await request(`${options.apiBase}/@admin/update-feed`, {
-            headers: {
-                "AdminKey": options.adminKey
+async function main() {
+    // Config log
+    log4js.configure({
+        appenders: {
+            console: {
+                type: "console"
             },
-            qs: {
-                "feed-uri": feedUri
+            file: {
+                type: "file",
+                filename: `${__dirname}/log/refresh_feeds.log`,
+                maxLogSize: 1 * 1024 * 1024,
             }
-        });
-    } catch (e) {
-        ++failedCount;
-        console.log(`Refresh ${feedUri} failed: ${e}`);
+        },
+        categories: {
+            default: {
+                appenders: ['console', 'file'],
+                level: options.logLevel
+            }
+        }
+    });
+    log = log4js.getLogger("feedreader.refres_feeds");
+
+    // Parse options.
+    var args = process.argv.slice(2);
+    for (var i = 0; i < args.length; ++i) {
+        switch (args[i]) {
+            case "-h": console.log("Usage: node refresh_feeds.js --admin-key <xx> --api-base <xxxx>"); process.exit(0);
+            case "--admin-key": options.adminKey = args[++i]; break;
+            case "--api-base": options.apiBase = args[++i]; break;
+            case "--log": options.logLevel = args[++i]; break;
+            default: throw `Unknown options '${args[i]}'`;
+        }
     }
+
+    // Reset log level.
+    log.level = options.logLevel;
+    
+    // Normalize api base.
+    for (var i = options.apiBase.length - 1; i >= 0; --i) {
+        if (options.apiBase[i] != '/') {
+            options.apiBase = options.apiBase.substr(0, i + 1);
+            break;
+        }
+    }
+
+    // Log api base.
+    log.info(`Refresh starts, apiBase: ${options.apiBase}`);
+    
+    // Get feed uri list.
+    var data = await request(`${options.apiBase}/@admin/get-feed-uri-list`, {
+        headers: {
+            "AdminKey": options.adminKey
+        }
+    });
+
+    // Refresh feed one by one.
+    var feedURIs = JSON.parse(data);
+    var failedCount = 0;
+    log.info(`${feedURIs.length} feeds need be refreshed.`);
+    for (var i in feedURIs) {
+        var feedUri = feedURIs[i];
+        try {
+            log.debug(`Refresh feed: ${feedUri}`);
+            await request(`${options.apiBase}/@admin/update-feed`, {
+                headers: {
+                    "AdminKey": options.adminKey
+                },
+                qs: {
+                    "feed-uri": feedUri
+                }
+            });
+        } catch (e) {
+            ++failedCount;
+            log.error(`Refresh ${feedUri} failed: ${e}`);
+        }
+    }
+    log.info(`Refresh finished, ${failedCount} feeds are failed to refresh.`);
 }
-console.log(`Refresh finished, ${failedCount} feeds are failed to refresh.`);
-})();
