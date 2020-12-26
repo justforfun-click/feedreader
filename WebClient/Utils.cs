9b8b7f7b0b3ed7d404333c2c4ae1024ba6124c23
xyb <xyb@xyb.name>
2020-12-26 11:41:38 -0800

    Always use https for feed icon and topic image uri. It will avoid the unsecure warning in browser.

---
+++ WebClient/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace FeedReader.WebClient
+{
+    public class Utils
+    {
+        public static string SafeImageUri(string uri)
+        {
+            if (string.IsNullOrWhiteSpace(uri) || uri.StartsWith("https://"))
+            {
+                return uri;
+            }
+            else
+            {
+                return string.Format("https://proxy.feedreader.org?url={0}", HttpUtility.UrlEncode(uri));
+            }
+        }
+    }
+}
