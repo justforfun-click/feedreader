deffe95b55e0fd600a71197135fc80bc4e5128aa
xyb <xyb@xyb.name>
2021-02-22 06:39:43 -0800

    Remove unused code

--- src/server/FeedReader.ServerCore/Extensions/EntityExtensions.cs
+++ src/server/FeedReader.ServerCore/Extensions/EntityExtensions.cs
@@ -29,19 +29,5 @@ namespace FeedReader.WebApi.Extensions
                 TopicPictureUri = feedItem.TopicPictureUri,
             };
         }
-
-        public static FeedItem CopyTo(this Backend.Share.Entities.FeedItemExEntity feedItemEx, FeedItem feedItem)
-        {
-            feedItem.Content = feedItemEx.Content;
-            feedItem.FeedIconUri = feedItemEx.FeedIconUri;
-            feedItem.FeedName = feedItemEx.FeedName;
-            feedItem.FeedUri = feedItemEx.FeedUri;
-            feedItem.PermentLink = feedItemEx.PermentLink;
-            feedItem.PubDate = feedItemEx.PubDate;
-            feedItem.Summary = feedItemEx.Summary;
-            feedItem.Title = feedItemEx.Title;
-            feedItem.TopicPictureUri = feedItemEx.TopicPictureUri;
-            return feedItem;
-        }
     }
 }
